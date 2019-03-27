e4a871c458bb983232f8f7d1d6f72e596c638b91
Andrew Bay <daddnbay@yahoo.com>
2019-03-27 16:49:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,18 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
 
-    
-
-
-    
+ def breed=(breed, mutt)
+   @breed = breed
+ end
+end
